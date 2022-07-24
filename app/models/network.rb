955069246d7259896adda5a1878d464742e3a085
Kai L <kailin8591@gmail.com>
2022-07-24 18:23:46 -0400

    never finishing

--- app/models/network.rb
+++ app/models/network.rb
@@ -1,3 +1,21 @@
 class Network < ActiveRecord::Base
-  
+  has_many :shows
+
+#shows: should return a list of all the show instances associated with the network.
+def shows
+  container = []
+  self.shows.map do |show|
+    if network.id == show.network_id
+      container << show
+    end
+  end
+  puts container.inspect
+  container
+end
+
+def sorry
+  "We're sorry about passing on John Mulaney's pilot"
+end
+
+
 end
--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,13 @@
 class Show < ActiveRecord::Base
-  
+    has_many :characters
+    has_many :actors, through: :characters
+    belongs_to :network
+
+  def actors_list
+    self.characters.collect do |s|
+      s.actor.full_name 
+      # this is calling the actor.rb's method full_name thats why it is actor.full_name
+    end
+  end
+
 end
