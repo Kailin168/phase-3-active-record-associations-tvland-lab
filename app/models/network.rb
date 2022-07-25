e158cc736a574dea0e79be0cff6cd3eab32cc13a
Kai L <kailin8591@gmail.com>
2022-07-25 09:00:26 -0400

    commit tree 63301b190035618fabb5e5be255e29162f14988b

--- app/models/network.rb
+++ app/models/network.rb
@@ -2,16 +2,16 @@ class Network < ActiveRecord::Base
   has_many :shows
 
 #shows: should return a list of all the show instances associated with the network.
-def shows
-  container = []
-  self.shows.map do |show|
-    if network.id == show.network_id
-      container << show
-    end
-  end
-  puts container.inspect
-  container
-end
+# def shows
+#   self.shows
+#   # container = []
+#   # self.shows.map do |show|
+#   #   if network.id == show.network_id
+#   #     container << show
+#   #   end
+#   # end
+#   # container
+# end
 
 def sorry
   "We're sorry about passing on John Mulaney's pilot"
